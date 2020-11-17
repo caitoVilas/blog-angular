ca98a8595f7b170a597455ce800952c68e87a854
caito <caitocd@gmail.com>
2020-11-16 21:08:14 -0300

    se realizador cambios en los slider y se creo modelo Pelicula

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { Pelicula } from '../../models/peliculas'
 
 @Component({
   selector: 'app-peliculas',
@@ -7,16 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  public peliculas: Array<any>;
+  public peliculas: Pelicula[];
+    
 
   public title: string;
 
   constructor() { 
     this.title = 'Peliculas';
     this.peliculas = [
-      {year: 2019,title: 'Spiderman 4', image: 'http://1.bp.blogspot.com/-_JFIkASEsLY/Uqv3gnL6UNI/AAAAAAAAQDA/9397w7ODk_w/w1200-h630-p-k-no-nu/Spider+man+4.jpg'},
-      {year: 2018,title: 'Los Vengadores Endgame', image: 'http://es.web.img3.acsta.net/newsv7/19/04/20/10/32/3283729.jpg'},
-      {year: 2015,title: 'Batman vs Superman', image: 'http://solismagazine.com/wp-content/uploads/2013/08/superman-batman-1.jpg'},
+      new Pelicula('Spiderman 4', 2019,'http://1.bp.blogspot.com/-_JFIkASEsLY/Uqv3gnL6UNI/AAAAAAAAQDA/9397w7ODk_w/w1200-h630-p-k-no-nu/Spider+man+4.jpg'),
+      new Pelicula('Los Vengadores Endgame',2018,'http://es.web.img3.acsta.net/newsv7/19/04/20/10/32/3283729.jpg'),
+      new Pelicula('Batman vs Superman',2015,'http://solismagazine.com/wp-content/uploads/2013/08/superman-batman-1.jpg')
     ]
   }
 
